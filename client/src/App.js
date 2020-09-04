4848d3273b9327e4b0d4b7582bb5081985a583a9
David Gold <davidgold@hey.com>
2020-09-04 11:25:56 -0400

    custom hook implemented

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import PlantList from "./components/PlantList";
 import ShoppingCart from "./components/ShoppingCart";
 import CheckoutForm from "./components/CheckoutForm";
+import useForm from "../src/hooks/useForm";
 
 import "./App.css";
 
